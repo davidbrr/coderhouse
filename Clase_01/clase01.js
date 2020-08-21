266c7aa3f0db8bbddc45496db2ff0a7ee4c3e4ec
Dave <dalbar04@gmail.com>
2020-08-20 21:39:37 -0500

    Add files via upload

---
+++ Clase_01/clase01.js
@@ -0,0 +1,21 @@
+//// Variables ///
+
+var saludo ="Hola coders"; 
+var numero_a = 1;
+var numero_b= 2;
+
+instituto ="coderhouse";
+
+//PROMPT es un cuadro de dialogo
+//ejemplo de Prompr
+var name = prompt("Ingresa tu nombre: "); // se mostrará una ventana y el valor escrito se guardará en la variable name
+
+console.log("esto es u ntexto en consola");
+
+console.info("otro texto info");
+
+console.warn("esto es un  advertencia");
+
+console.error("esto es un error");
+
+alert("esto es una alerta");
